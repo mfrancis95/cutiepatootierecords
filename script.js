3d2e2cc713ab21698ab32f3b8717e8ac5fd8abc2
mfrancis95 <4574480+mfrancis95@users.noreply.github.com>
2023-01-10 10:50:41 -0500

    Add drop shadow effect and use CSS variables everywhere

--- script.js
+++ script.js
@@ -39,28 +39,32 @@ const variants = [
 ]
 
 window.addEventListener('load', () => {
+    const fragment = new DocumentFragment();
     for (let i = 0; i < variants.length; i++) {
         const variant = variants[i];
-        for (let j = 0; j < randomRange(variant.count.min, variant.count.max); j++) {
+        for (let j = 0; j < randomRange(...Object.values(variant.count)); j++) {
             const element = document.createElement('span');
-            element.className = 'variant' + (i + 1);
-            element.style.animation = `float ${randomRange(2, 10)}s ease-in-out -${randomRange(0, 2)}s infinite`;
-            element.style.filter = `blur(${randomRange(0, 8)}px)`;
-            element.style.opacity = randomRange(0.5, 1);
-            element.style.setProperty('--position-x', randomRange(-25, 100) + 'vw');
-            element.style.setProperty('--position-y', randomRange(-25, 100) + 'vh');
+            element.className = `variant${i + 1}`;
+            element.style.left = `${randomRange(-25, 100)}%`;
+            element.style.top = `${randomRange(-25, 100)}%`;
+            element.style.setProperty('--blur-radius', `${randomRange(0, 8)}px`);
+            element.style.setProperty('--drop-shadow-radius', `${randomRange(0, 20)}px`);
+            element.style.setProperty('--float-delay', `-${randomRange(0, 2)}s`);
+            element.style.setProperty('--float-duration', `${randomRange(2, 10)}s`);
+            element.style.setProperty('--opacity', randomRange(0.5, 1));
             element.style.setProperty('--rotation', `${randomSign()}${randomRange(0, 0.01)}turn`);
             if (variant.link) {
                 const anchor = document.createElement('a');
                 anchor.href = variant.link;
                 anchor.appendChild(element);
-                document.body.appendChild(anchor);
+                fragment.appendChild(anchor);
             }
             else {
-                document.body.appendChild(element);
+                fragment.appendChild(element);
             }
         }
     }
+    document.body.appendChild(fragment);
 });
 
 function randomRange(min, max) {
