0e3b11a9e2924261fdffe0b2df9c2f3741afb463
mfrancis95 <mfrancis95@users.noreply.github.com>
2026-05-09 20:05:24 -0400

    Add 3D scrolling

--- script.js
+++ script.js
@@ -1,76 +1,46 @@
-const variants = [
-    {
-        count: {
-            min: 25,
-            max: 75
-        }
-    },
-    {
-        count: {
-            min: 25,
-            max: 75
-        }
-    },
-    {
-        count: {
-            min: 25,
-            max: 75
-        }
-    },
-    {
-        count: {
-            min: 25,
-            max: 75
-        }
-    },
-    {
-        count: {
-            min: 25,
-            max: 75
-        }
-    },
-    {
-        count: {
-            min: 5,
-            max: 25
-        },
-        link: 'https://www.instagram.com/cutiepatootierecords'
-    }
-]
-
-window.addEventListener('load', () => {
-    const fragment = new DocumentFragment();
-    for (let i = 0; i < variants.length; i++) {
-        const variant = variants[i];
-        for (let j = 0; j < randomRange(...Object.values(variant.count)); j++) {
-            const element = document.createElement('span');
-            element.className = `variant${i + 1}`;
-            element.style.left = `${randomRange(-25, 100)}%`;
-            element.style.top = `${randomRange(-25, 100)}%`;
-            element.style.setProperty('--blur-radius', `${randomRange(0, 8)}px`);
-            element.style.setProperty('--drop-shadow-radius', `${randomRange(0, 20)}px`);
-            element.style.setProperty('--float-delay', `-${randomRange(0, 2)}s`);
-            element.style.setProperty('--float-duration', `${randomRange(2, 10)}s`);
-            element.style.setProperty('--opacity', randomRange(0.5, 1));
-            element.style.setProperty('--rotation', `${randomSign()}${randomRange(0, 0.01)}turn`);
-            if (variant.link) {
-                const anchor = document.createElement('a');
-                anchor.href = variant.link;
-                anchor.appendChild(element);
-                fragment.appendChild(anchor);
-            }
-            else {
-                fragment.appendChild(element);
-            }
-        }
-    }
-    document.body.appendChild(fragment);
-});
-
 function randomRange(min, max) {
+    if (max === undefined) {
+        max = min;
+        min = 0;
+    }
     return min + Math.random() * (max - min);
 }
 
-function randomSign() {
-    return Math.random() < 0.5 ? '' : '-';
-}
+fetch('variants.json').then(response => response.json()).then(variants => {
+    const fragment = new DocumentFragment();
+    variants.forEach(({ color, content, count, fontSize, link }) => {
+        for (let i = 0; i < randomRange(count.min, count.max); i++) {
+            let element;
+            if (link) {
+                element = document.createElement('a');
+                element.href = link;
+            }
+            else {
+                element = document.createElement('span');
+            }
+            element.style.fontSize = fontSize;
+            element.style.left = `${randomRange(-25, 100)}%`;
+            element.style.top = `${randomRange(-25, 100)}%`;
+            element.style.setProperty('--blur-radius', `${randomRange(8)}px`);
+            element.style.setProperty('--color', color);
+            element.style.setProperty('--drop-shadow-radius', `${randomRange(20)}px`);
+            element.style.setProperty('--fade-delay', `${randomRange(2)}s`);
+            element.style.setProperty('--float-delay', `-${randomRange(2)}s`);
+            element.style.setProperty('--float-duration', `${randomRange(2, 10)}s`);
+            element.style.setProperty('--opacity', randomRange(0.5, 1));
+            element.style.setProperty('--rotation', `${randomRange(-0.01, 0.01)}turn`);
+            element.style.setProperty('--z-position', `${randomRange(-500, 500)}px`);
+            element.textContent = content;
+            fragment.appendChild(element);
+        }
+    });
+    document.body.appendChild(fragment);
+});
+
+let deltaY = 0;
+
+window.addEventListener('wheel', event => {
+    requestAnimationFrame(() => {
+        document.body.style.setProperty('--delta-y', `${deltaY += event.deltaY}px`);
+    })
+}, { passive: true });
